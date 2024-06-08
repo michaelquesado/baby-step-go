f21ad52a50255eff5a60d4da927ab45bf7181fac
michaelquesado <esquesado@gmail.com>
2024-06-08 13:46:32 -0300

    encode response to json and include cep

--- http/simple-server/main.go
+++ http/simple-server/main.go
@@ -64,7 +64,7 @@ func helloWordHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(string(parseStrucToJson(cepRes))))
+	json.NewEncoder(w).Encode(cepRes)
 }
 
 func parseStrucToJson(s any) []byte {
