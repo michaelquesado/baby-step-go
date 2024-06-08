2f09dc61dac4c09065c3a7eb6bccb71882df51ea
michaelquesado <esquesado@gmail.com>
2024-06-08 11:53:00 -0300

    include struct to cep and validation response

--- http/simple-server/main.go
+++ http/simple-server/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
 
 	http.HandleFunc("/", helloWordHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+type Validation struct {
+	Error string `json:"error"`
+}
+
+func NewValidation(error string) *Validation {
+	v := Validation{Error: error}
+	return &v
+}
+
+type CepResponse struct {
+	Msg string `json:"msg"`
+}
+
+func NewCepResponse(msg string) *CepResponse {
+	c := CepResponse{Msg: msg}
+	return &c
 }
 
 func helloWordHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +38,21 @@ func helloWordHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte("{'msg': 'Hello, Word!'}"))
+	cep := r.URL.Query().Get("cep")
+	if len(cep) < 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(string(parseStrucToJson(NewValidation("Invalid cep")))))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(string(parseStrucToJson(NewCepResponse("Hello, World!")))))
+}
+
+func parseStrucToJson(s any) []byte {
+	res, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+	return res
 }
