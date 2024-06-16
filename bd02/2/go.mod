68f6d734d65a696bed01db2cb7857e49163aa396
michaelquesado <esquesado@gmail.com>
2024-06-16 20:15:39 -0300

    start with gorm

---
+++ bd02/2/go.mod
@@ -0,0 +1,14 @@
+module github.com/michaelquesado/baby-step-go/tree/main/bd02/02
+
+go 1.22.4
+
+require (
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ bd02/2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Company struct {
+	Id   int `gorm:"primaryKey;autoIncrement:true"`
+	Name string
+	CNPJ string
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/gotest"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Company{})
+
+	// db.Create(&Company{
+	// 	Name: "UPA",
+	// 	CNPJ: "87278810001/02",
+	// })
+	companies := []Company{
+		{Name: "FNJ", CNPJ: "0019283820001/02"},
+		{Name: "Paraiso", CNPJ: "991923902010001/93"},
+	}
+	db.Create(&companies)
+}
