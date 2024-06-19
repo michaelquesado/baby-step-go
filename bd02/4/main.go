813f221ebec1e20cc6a9639b1e84aa79b85bb98d
michaelquesado <esquesado@gmail.com>
2024-06-18 21:43:31 -0300

    setup transaction with gorm

--- bd02/4/main.go
+++ bd02/4/main.go
@@ -5,10 +5,27 @@ import (
 	"gorm.io/gorm"
 )
 
+type Studant struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+	gorm.Model
+}
+
 func main() {
 	dsn := "root:root@tcp(localhost:3306)/gotest?charset=utf8mb4&parseTime=true&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(db)
 	}
+
+	db.AutoMigrate(&Studant{})
+
+	tx := db.Begin()
+	err = tx.Debug().Create(&Studant{Name: "Carla"}).Error
+	if err != nil {
+		tx.Debug().Rollback()
+		panic(err)
+	}
+	tx.Debug().Commit()
+
 }
