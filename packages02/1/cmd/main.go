6401932440e77d9d8903f498ea9ff36974979ed0
michaelquesado <esquesado@gmail.com>
2024-06-18 21:59:06 -0300

    define a math package

--- packages02/1/cmd/main.go
+++ packages02/1/cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/michaelquesado/baby-step-go/packages02/1/math"
+)
 
 func main() {
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
 	fmt.Println("Hello, world!")
 }
