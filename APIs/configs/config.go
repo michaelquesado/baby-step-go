e1bbc97adaed7bab09d1d8d83cad0a8db548ef1f
michaelquesado <esquesado@gmail.com>
2024-06-22 12:00:57 -0300

    load configs file with viper

--- APIs/configs/config.go
+++ APIs/configs/config.go
@@ -1,21 +1,39 @@
 package configs
 
-import "fmt"
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
 
 var configs *conf
 
 type conf struct {
-	DBDriver      string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPassword    string
-	DBName        string
-	WebServerPort string
-	JWTSecret     string
-	JWTExperesIn  int
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mpastructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWTExperesIn  int    `mapstructure:"JWT_EXPERESIN"`
+	TokenAuth     *jwtauth.JWTAuth
 }
 
 func LoadConfig(path string) (*conf, error) {
-	return nil, fmt.Errorf("Fail to load configs")
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&configs)
+	if err != nil {
+		panic(err)
+	}
+	configs.TokenAuth = jwtauth.New("HS256", []byte(configs.JWTSecret), nil)
+	return configs, nil
 }
