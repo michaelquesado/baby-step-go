343bbe97bbce2e58669882a6695606f2c9ddfba7
michaelquesado <esquesado@gmail.com>
2024-06-30 19:26:20 -0300

    concurrency with atomic

--- multithreading/mutex-atomic/main.go
+++ multithreading/mutex-atomic/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"net/http"
-	"sync"
+	"sync/atomic"
 )
 
 var number int64 = 0
 
 func main() {
-	m := sync.Mutex{}
+	// m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		m.Lock()
-		number++
-		m.Unlock()
+		// m.Lock()
+		// number++
+		atomic.AddInt64(&number, 1)
+		// m.Unlock()
 		w.WriteHeader(201)
 	})
 	http.ListenAndServe(":8080", nil)
